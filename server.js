2eae3211642f72c13f9c97cc1ca3b99f54fe4a25
Evan Lucas <evanlucas@me.com>
2013-10-07 18:02:59 -0500

    Added cluster support

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ var mongoose = require('mongoose')
   , fs = require('fs')
   , path = require('path')
   , helper = require('./controllers/helper')
+  , cluster = require('cluster')
+  , cpuLen = require('os').cpus().length
+  , RedisStore = require('socket.io/lib/stores/redis')
+  , redis = require('socket.io/node_modules/redis')
+  , pub = redis.createClient()
+  , sub = redis.createClient()
+  , client = redis.createClient()
+
+
 
 /*!
  * Make sure we are root (if we're using port <= 1024)
@@ -21,105 +30,114 @@ if (config.proxySPort <= 1024) {
   }
 }
 
-/*!
- * Verify that the config has valid parameters for the given environment/context in which we have been run
- *
- * MAKE SURE IT IS VALID....IT WILL THROW AN ERROR
- */
-helper.checkConfig()
-
-/*!
- * Connect to the database
- */
-mongoose.connect(config.db)
-
-/*!
- * Initialize and register our schema
- */
-fs.readdirSync(path.join(__dirname, 'models')).forEach(function(f) {
-  if (path.extname(f) === '.js') {
-    require(path.join(__dirname, 'models', f))
+if (cluster.isMaster) {
+  for (var i=0; i<cpuLen; i++) {
+    cluster.fork()
+    cluster.on('exit', function(worker) {
+      cluster.fork()
+    })
   }
-})
+} else {
+  /*!
+   * Verify that the config has valid parameters for the given environment/context in which we have been run
+   *
+   * MAKE SURE IT IS VALID....IT WILL THROW AN ERROR
+   */
+  helper.checkConfig()
 
-/*!
- * Start the proxy server
- */
-var proxyServer = new (require('./controllers/proxyserver'))
+  /*!
+   * Connect to the database
+   */
+  mongoose.connect(config.db)
 
-/*!
- * Connect to our socket client
- * Fail and exit if we catch an error or the server closes
- * 
- * TODO:
- *    Find a way to check a couple of times prior to exiting
- *    since the socket server is managed by launchd also
- *
- * It is setup this way so that when we clone, or pull, or install
- * depends, we are not doing so as root, but as the user who actually
- * runs the applications. This is done for privilege separation
- *
- *
- */
-var socketclient = new (require('./utils/socketclient'))
-
-
-socketclient
-  .connect()
-  .on('connect', function() {
-    /*!
-     * We connected, initialize express
-     */
-    var app = express()
-
-    app.config = config
-    
-    
-    /**
-     * Create separate http server for the sake of socket.io
-     */
-    var server = require('http').createServer(app);
-    
-    /**
-     * Start listening for socket messages
-     */
-    var io = require('socket.io').listen(server);
-    
-    /*!
-     * Require our configuration
-     */
-    var exp = require('./config/express')(app, config, io)
-    
-    var passport = exp.passport
-      , socks = exp.socks
-    /*!
-     * Setup routes and API
-     */
-    require('./config/router')(app, proxyServer, socketclient, passport)
-      
-    var port = config.adminPort
-    
-    /*!
-     * Start listening
-     */  
-    server.listen(port)
-    
-    new (require('./config/sockets'))(socks, io)
-    
-    logger.info('['+env+'] - Listening on port ['+port+']')
-  })
-  .on('error', function(e) {
-    logger.info('SocketClient caught an error')
-    logger.error(e)
-    process.exit(1)
-  })
-  .on('close', function(hasError) {
-    logger.info('SocketClient was closed')
-    if (hasError) {
-      logger.warn('SocketClient closed due to an error')
-      process.exit(1)
-    } else {
-      logger.info('SocketClient closed in a clean manner')
-      process.exit()
+  /*!
+   * Initialize and register our schema
+   */
+  fs.readdirSync(path.join(__dirname, 'models')).forEach(function(f) {
+    if (path.extname(f) === '.js') {
+      require(path.join(__dirname, 'models', f))
     }
   })
+
+  /*!
+   * Start the proxy server
+   */
+  var proxyServer = new (require('./controllers/proxyserver'))
+
+  /*!
+   * Connect to our socket client
+   * Fail and exit if we catch an error or the server closes
+   *
+   * TODO:
+   *    Find a way to check a couple of times prior to exiting
+   *    since the socket server is managed by launchd also
+   *
+   * It is setup this way so that when we clone, or pull, or install
+   * depends, we are not doing so as root, but as the user who actually
+   * runs the applications. This is done for privilege separation
+   *
+   *
+   */
+  var socketclient = new (require('./utils/socketclient'))
+
+
+  socketclient
+    .connect()
+    .on('connect', function() {
+      /*!
+       * We connected, initialize express
+       */
+      var app = express()
+
+      app.config = config
+
+
+      /**
+       * Create separate http server for the sake of socket.io
+       */
+      var server = require('http').createServer(app);
+
+      /**
+       * Start listening for socket messages
+       */
+      var io = require('socket.io').listen(server);
+
+      /*!
+       * Require our configuration
+       */
+      var exp = require('./config/express')(app, config, io)
+
+      var passport = exp.passport
+        , socks = exp.socks
+      /*!
+       * Setup routes and API
+       */
+      require('./config/router')(app, proxyServer, socketclient, passport)
+
+      var port = config.adminPort
+
+      /*!
+       * Start listening
+       */
+      server.listen(port)
+
+      new (require('./config/sockets'))(socks, io)
+
+      logger.info('['+cluster.worker.id+']'+'['+env+'] - Listening on port ['+port+']')
+    })
+    .on('error', function(e) {
+      logger.info('SocketClient caught an error')
+      logger.error(e)
+      process.exit(1)
+    })
+    .on('close', function(hasError) {
+      logger.info('SocketClient was closed')
+      if (hasError) {
+        logger.warn('SocketClient closed due to an error')
+        process.exit(1)
+      } else {
+        logger.info('SocketClient closed in a clean manner')
+        process.exit()
+      }
+    })
+}
